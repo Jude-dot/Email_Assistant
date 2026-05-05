260c57d72caaf58f223b4d77cac0417ef43a7279
Jude Samuel <judesamuelo60@gmail.com>
2026-05-05 03:10:03 +0100

    Modified fonts in send_emai file

--- send_email.py
+++ send_email.py
@@ -11,29 +11,29 @@ SMTP_PORT = 587
 EMAIL_ADDRESS = os.getenv("EMAIL_ADDRESS")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD")
 
-def send_email(Recipient_email, Subject, Body):
+def send_email(recipient_email, subject, body):
     try:
         msg = MIMEMultipart()
         msg['From'] = EMAIL_ADDRESS
-        msg['To'] = Recipient_email
-        msg['Subject'] = Subject
+        msg['To'] = recipient_email
+        msg['Subject'] = subject
 
-        msg.attach(MIMEText(Body, 'plain'))
+        msg.attach(MIMEText(body, 'plain'))
 
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
             server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            server.sendmail(EMAIL_ADDRESS, Recipient_email, msg.as_string())
+            server.sendmail(EMAIL_ADDRESS, recipient_email, msg.as_string())
 
         print("Email sent successfully!")
     except Exception as e:
         print(f"Error sending mail: {e}")
 
 if __name__ == "__main__":
-    recipient = "judesamuelo60@gmail.com"
-    subject = "Email Test from Python"
-    body = "This is a test email sent from you to you using python"
-    send_email(recipient, subject, body)
+    Recipient = "judesamuelo60@gmail.com"
+    Subject = "Email Test from Python"
+    Body = "This is a test email sent from you to you using python"
+    send_email(Recipient, Subject, Body)
 
 
 
